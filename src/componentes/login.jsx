b9eb419a185d7390fff4445173183dcc6cf07ad7
Tomas Morales <tomasmorales004@gmail.com>
2024-11-02 14:26:13 -0300

    s

--- src/componentes/login.jsx
+++ src/componentes/login.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importa useNavigate
-import { users } from './users'; // Importa el archivo de usuarios
+import { useNavigate } from 'react-router-dom'; 
+import { users } from './users'; 
 
 function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate(); // Inicializa useNavigate para la redirección
+  const navigate = useNavigate(); 
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-    // Verifica si los datos coinciden con algún usuario en la lista
+    /
     const user = users.find(
       (user) => user.email === email && user.password === password
     );
 
     if (user) {
       alert('Inicio de sesión exitoso');
-      localStorage.setItem('isAuthenticated', true); // Autenticación en localStorage
-      navigate('/cuentas'); // Redirige a la ruta protegida
+      localStorage.setItem('isAuthenticated', true); 
+      navigate('/cuentas'); 
     } else {
       setError('Usuario o contraseña incorrectos');
     }
